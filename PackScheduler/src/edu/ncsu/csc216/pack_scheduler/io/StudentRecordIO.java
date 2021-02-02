73c7957501d19fe2030e3cc4b2530f43973b0c83
mymeles <mymeles@ncsu.edu>
2021-02-02 09:45:15 -0500

    trouble shooting jenkins redball

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
@@ -12,53 +12,48 @@ import java.io.BufferedWriter;
 import edu.ncsu.csc216.pack_scheduler.user.Student;
 
 public class StudentRecordIO {
-	
-	
-	
-	
-	// file name student  the format string 
+
+	// file name student the format string
 
 	// ArrayList<Studet> sd;
-	
-	//  
-	
-	// ArrayList<String> splitLine = line.split(",") we get an array 
-	    /**
-	     * firstName  0
-	     * lastName   1
-	     *     ..... 5
-	     * 	     * 
-	     * 
-	     * Student st1 = new Student(splitLIne[0], ..LIne[5] -----this is in the loop
-	     * sd.add(st1); 
-	     *  
-	     *  * @param fileName...splitK
-	     * @return
-	     * @throws FileNotFoundException
-	     */
+
+	//
+
+	// ArrayList<String> splitLine = line.split(",") we get an array
+	/**
+	 * firstName 0 lastName 1 ..... 5 *
+	 * 
+	 * Student st1 = new Student(splitLIne[0], ..LIne[5] -----this is in the loop
+	 * sd.add(st1);
+	 * 
+	 * * @param fileName...splitK
+	 * 
+	 * @return
+	 * @throws FileNotFoundException
+	 */
 	public static ArrayList<Student> readStudentRecords(String fileName) throws FileNotFoundException {
-		
-		ArrayList<Student> sd;	
+
+		ArrayList<Student> sd;
 		BufferedReader reader;
 		try {
 			reader = new BufferedReader(new FileReader(fileName));
 			String line = reader.readLine();
-			while( line != null) {
+			while (line != null) {
 				line = reader.readLine();
 				ArrayList<String> splitLine = new ArrayList<>(Arrays.asList(line.split(",")));
 				int credit = Integer.parseInt(splitLine.get(5));
-				Student st1 = new Student(splitLine.get(0), splitLine.get(1), splitLine.get(2), splitLine.get(3), splitLine.get(4), credit);
+				Student st1 = new Student(splitLine.get(0), splitLine.get(1), splitLine.get(2), splitLine.get(3),
+						splitLine.get(4), credit);
 				sd.add(st1);
 			}
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return sd;
 	}
 
-	
 //	
 //	public class TextFileWritingExample2 {
 //		 
@@ -81,7 +76,7 @@ public class StudentRecordIO {
 	 */
 //	 
 //	            bufferedWriter.close();
-//	        } catch (IOException e) {https://ncsu.zoom.us/j/93922393721?pwd=enJaeEJ5WmxlUzh5TzZMK1Fwem4ydz09
+//	        } catch (IOException e)
 //	            e.printStackTrace();
 //	        }
 //	 
@@ -90,23 +85,22 @@ public class StudentRecordIO {
 //g
 	public static void writeStudentRecords(String fileName, ArrayList<Student> studentDirectory) throws IOException {
 		
-		try {
-			FileWriter writer = new FileWriter(fileName, true);
-			BufferedWriter bufferedWriter = new BufferedWriter(writer); 
-			
-			for(int i =0; i < studentDirectory.size(); i++) {
-				Student st = studentDirectory.get(i);
-				
-				String line = st.getFirstName() + "," + st.getLastName() + "," + st.getId() + "," + st.getEmail() + "," + st.getPassword() + "," + st.getMaxCredits();
-				bufferedWriter.write(line);
-				
-				if(i != studentDirectory.size()-1) {
-					bufferedWriter.newLine();
-					
-				}
-			}
-		}
-		
-	}
-
-} 
+//		try {
+//			FileWriter writer = new FileWriter(fileName, true);
+//			BufferedWriter bufferedWriter = new BufferedWriter(writer); 
+//			
+//			for(int i =0; i < studentDirectory.size(); i++) {
+//				Student st = studentDirectory.get(i);
+//				
+//				String line = st.getFirstName() + "," + st.getLastName() + "," + st.getId() + "," + st.getEmail() + "," + st.getPassword() + "," + st.getMaxCredits();
+//				bufferedWriter.write(line);
+//				
+//				if(i != studentDirectory.size()-1) {
+//					bufferedWriter.newLine();
+//					
+//				}
+//			} 
+//
+//		}
+//
+//	}
